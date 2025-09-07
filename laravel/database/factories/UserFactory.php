8fb1c3eeb78d74ecc9980bdae64c1455d1fd77b0
西原　ももか <nishihara@Macintosh.local>
2025-09-07 17:30:34 +0900

    LoginRequest作成/バリデーションメッセージ追加

--- laravel/database/factories/UserFactory.php
+++ laravel/database/factories/UserFactory.php
@@ -25,10 +25,11 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'email' => 'aaa@gmail.com',
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => 'password123',
             'remember_token' => Str::random(10),
+            'role' => 'member'
         ];
     }
 
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\AuthController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,10 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::get('/test', function () {
-    return response()->json(['message' => 'Hello from Laravel']);
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::post('/login', [AuthController::class, 'login']);
