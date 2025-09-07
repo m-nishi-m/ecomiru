f9bc7a44ea37bcca19b36c972775a89ff0f7297b
西原　ももか <nishihara@Macintosh.local>
2025-09-07 17:49:35 +0900

    api/UserFactory.php不要な記述削除

--- laravel/database/factories/UserFactory.php
+++ laravel/database/factories/UserFactory.php
@@ -25,9 +25,9 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
-            'email' => 'aaa@gmail.com',
+            'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => 'password123',
+            'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
             'role' => 'member'
         ];
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -15,8 +15,4 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
 Route::post('/login', [AuthController::class, 'login']);
