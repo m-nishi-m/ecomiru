53287e20d502d59b5cd0c91f6ad375b07c8ba0d9
西原　ももか <nishihara@Macintosh.local>
2025-09-20 18:03:48 +0900

    不要な行を削除

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -15,7 +15,6 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::delete('/logout', [AuthController::class, 'logout']);
